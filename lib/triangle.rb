136e4bd17c80f4058e85f6aa2540a5559d1c8cfa
Andrew Ribas <andrew.j.ribas@gmail.com>
2020-07-23 17:52:09 +0000

    Automatically backed up by Learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -13,7 +13,7 @@ class Triangle
   def kind
     if @side_a == @side_b && @side_b == @side_c
       return :equilateral
-    elsif @side_a == @side_b || @side_b == @side_c
+    elsif @side_a == @side_b || @side_a == @side_c || @side_b == @side_c
       return :isosceles
     end
 
