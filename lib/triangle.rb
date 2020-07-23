c72f6fe38327e41100d4a42781e9a025fe0ac41e
Andrew Ribas <andrew.j.ribas@gmail.com>
2020-07-23 18:00:00 +0000

    Automatically backed up by Learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -16,7 +16,7 @@ class Triangle
       return :equilateral
     elsif @side_a == @side_b || @side_a == @side_c || @side_b == @side_c
       return :isosceles
-    elsif @side_a + @side_b > @side_c && @side_b + @side_c > @side_a && @side_c + @side_a > @side_b && !(@sides.include(0))
+    elsif @side_a + @side_b > @side_c && @side_b + @side_c > @side_a && @side_c + @side_a > @side_b && !(@sides.include?(0))
       return :scalene
     else
       raise TriangleError
